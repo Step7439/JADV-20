02045b859461e2a19890dbf57b77dd80d6698d40
Step7439 <zamashikovs@mail.ru>
2022-08-06 23:03:32 +0900

    first

--- src/main/java/ThreadAndRunnable/Main.java
+++ src/main/java/ThreadAndRunnable/Main.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        int t;
         String[] texts = new String[25];
         List<Thread> list = new ArrayList<>();
 
