0b865e8a07f12800d20d6bb7577435add9c1a7d6
Step7439 <zamashikovs@mail.ru>
2022-08-17 23:18:14 +0900

    MyQueue Edite

--- src/main/java/MyQueue/Main.java
+++ src/main/java/MyQueue/Main.java
@@ -5,14 +5,14 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
     public static int CALL = 60;
+
     public static void main(String[] args) {
         Queue<Integer> ATC = new ConcurrentLinkedQueue<>();
 
-
         new Thread(() -> {
-            for (int i = 0; i < CALL; i++){
+            for (int i = 1; i < CALL; i++) {
                 ATC.add(i);
-                System.out.println("Поступил звонок !" + i);
+                System.out.println("Поступил звонок  " + i + " " + " !");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -22,9 +22,10 @@ public class Main {
         }).start();
 
         new Thread(() -> {
-            for (int i = 0; i < CALL; i++) {
-                System.out.println("Оператор 1 обработал звонок !" + " " + i);
-                ATC.poll();
+            for (int i = 0; i <= CALL; i++) {
+                if (!ATC.isEmpty()) {
+                    System.out.println("Оператор 1 обработал звонок ! " + " " + ATC.poll());
+                }
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
@@ -35,9 +36,10 @@ public class Main {
         ).start();
 
         new Thread(() -> {
-            for (int i = 0; i < CALL; i++) {
-                System.out.println("Оператор 2 обработал звонок !" + " " + i);
-                ATC.poll();
+            for (int i = 0; i <= CALL; i++) {
+                if (!ATC.isEmpty()) {
+                    System.out.println("Оператор 2 обработал звонок ! " + " " + ATC.poll());
+                }
                 try {
                     Thread.sleep(4000);
                 } catch (InterruptedException e) {
